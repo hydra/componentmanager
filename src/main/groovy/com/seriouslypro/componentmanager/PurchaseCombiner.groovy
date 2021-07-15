d2febab9a7105e3779a49b167c34ae7b88bf95be
Dominic Clifton <me@dominicclifton.name>
2021-07-15 20:59:46 +0200

    Initial commit

---
+++ src/main/groovy/com/seriouslypro/componentmanager/PurchaseCombiner.groovy
@@ -0,0 +1,29 @@
+package com.seriouslypro.componentmanager
+
+import groovy.cli.picocli.CliBuilder
+import groovy.cli.picocli.OptionAccessor
+
+class PurchaseCombiner {
+
+    public static void main(String [] args) {
+        processArgs(args)
+    }
+
+    static def processArgs(String[] args) {
+        CliBuilder builder = new CliBuilder(usage: 'purchasecombiner')
+        builder.v('version')
+
+        OptionAccessor options = builder.parse(args)
+
+        if (!options || options.getParseResult().originalArgs().size() == 0) {
+            about()
+            builder.usage()
+            System.exit(-1)
+        }
+    }
+
+    private static void about() {
+        System.out.println('PurchaseCombiner (C) 2021 Dominic Clifton')
+        System.out.println('Written by Dominic Clifton')
+    }
+}
---
+++ src/test/groovy/com/seriouslypro/componentmanager/TestSpec.groovy
@@ -0,0 +1,11 @@
+package com.seriouslypro.componentmanager
+
+import spock.lang.Specification
+
+class TestSpec extends Specification {
+
+    def 'true'() {
+        expect:
+            true
+    }
+}
